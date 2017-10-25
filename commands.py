54345fcf7427b82e14a54c98dd72cfb9987f6cb2
Evan Wappel <ewappel@gmail.com>
2017-10-25 09:18:19 -0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -6,4 +6,21 @@ rails server
 #spring stop
 
 rails generate controller Pages
+#app/controllers/pages_controller.rb
+class PagesController < ApplicationController
+  def home
+  end
+end
 
+#config/routes.rb
+Rails.application.routes.draw do
+  get 'welcome' => 'pages#home'
+end
+
+#app/views/pages/home.html.erb (create)
+<div class="main">
+  <div class="container">
+    <h1>Hello my name is Pewt</h1>
+    <p>I make Rails apps.</p>
+  </div>
+</div>
